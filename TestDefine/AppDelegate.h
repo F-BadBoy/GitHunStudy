a0daff00e2fb51a4f1ad6885ef5277b0dd576f24
“ab049787” <ab049787fengyinan@ab-insurance.com>
2016-01-25 14:05:46 +0800

    Initial Commit

---
+++ TestDefine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDefine
+//
+//  Created by 冯一男 on 16/1/25.
+//  Copyright © 2016年 冯一男. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDefine/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestDefine
+//
+//  Created by 冯一男 on 16/1/25.
+//  Copyright © 2016年 冯一男. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
